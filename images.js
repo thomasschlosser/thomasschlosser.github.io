82587d9b12850b73f57dc8bc5471b761bea60f9a
Laura Schlosser <laura@schlosserzone.de>
2020-07-20 13:04:18 +0200

    Added article creasta-pietrei-craiului-2015 and updated posts page

--- images.js
+++ images.js
@@ -11,20 +11,43 @@ function displayImage(imageNumber) {
         }
     }
 
-    // Storing HTML code block in a variable
-    var codeBlock1 =
-    '<img id="' + imageNumber +  '" class="img-fluid img-thumbnail ' + size + ' "' +
-                'data-toggle="modal" data-target="#' + imageNumber + 'Modal"/>' +
+    if (document.getElementById(imageNumber + 'div').hasAttribute('noSize')) {
+        if (document.getElementById(imageNumber + 'div').getAttribute('noSize')=='true') {
+            size = '';
+        }
+    }
+
+    var captionCode = '';
+    var figureSize = '';
+    if (document.getElementById(imageNumber + 'div').hasAttribute('caption')) {
+        var captionText = document.getElementById(imageNumber + 'div').getAttribute('caption');
+        captionCode = '<figcaption class="figure-caption text-center">' + captionText + '</figcaption>'
+        figureSize = size;
+        size = '';
+    }
+
+    var imageCode = '<img id="' + imageNumber +  '" class="img-fluid ' + size + ' "' +
+                'data-toggle="modal" data-target="#' + imageNumber + 'Modal" alt=""/>';
+
+    var modalCode =
     '<div class="modal fade" id="' + imageNumber + 'Modal" tabindex="-1" role="dialog" ' +
-                'aria-labelledby="' + imageNumber + 'ModalLabel" aria-hidden="true">' +
-    '    <div class="modal-dialog modal-dialog-centered modal-' + modal + '" role="document">' +
-    '        <div class="modal-content">' +
-    '            <div class="modal-body">' +
-    '                <img id="' + imageNumber + 'ModalDisplay" class="img-fluid"/>' +
-    '            </div>' +
-    '        </div>' +
-    '    </div>' +
-    '</div>';
+                    'aria-labelledby="' + imageNumber + 'ModalLabel" aria-hidden="true">' +
+        '    <div class="modal-dialog modal-dialog-centered modal-' + modal + '" role="document">' +
+        '        <div class="modal-content">' +
+        '            <div class="modal-body">' +
+        '                <img id="' + imageNumber + 'ModalDisplay" class="img-fluid"/>' +
+        '            </div>' +
+        '        </div>' +
+        '    </div>' +
+        '</div>';
+
+    // Storing HTML code block in a variable
+    var codeBlock1 = '';
+    if (captionCode == '') {
+        codeBlock1 = imageCode + modalCode;
+    } else {
+        codeBlock1 = '<figure class="img-thumbnail ' + figureSize +'">' + imageCode + captionCode + '</figure>' + modalCode;
+    }
 
     // Inserting the code block to wrapper element
     document.getElementById(imageNumber + 'div').innerHTML = codeBlock1;
